1f4a82ef1624e45652e14ffc1804439e1946a19b
talatiandtalati <tina@talatiandtalati.com>
2024-01-29 22:31:53 +0530

    added data

--- src/components/InvitationCard.jsx
+++ src/components/InvitationCard.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef,useState } from "react";
 import video from "./assets/invitation.mp4";
-import MobVideo from "./assets/invitationMobile.mp4";
+import VideoWebM from "./assets/invitation .webm";
 import { motion, useAnimationControls } from "framer-motion";
 import { useMediaQuery } from "react-responsive";
 import data from './data.json'
@@ -24,12 +24,14 @@ const InvitationCard = () => {
     <div className="w-full h-full">
       <video
         ref={v}
-        muted
+        autoPlay
         className="sm:w-full w-screen h-full object-cover"
         onEnded={() => controls.start({ opacity: 1, scale: 1 })}
-        src={video}
-        type="video/mp4"
+        muted 
+        controls
       >
+         <source src={video} type="video/mp4"/>
+         <source src={VideoWebM} type="video/webm"/>
         Your browser does not support the video tag.
       </video>
       <div className="w-full h-full absolute top-0 left-0 flex justify-center items-center">
@@ -39,27 +41,28 @@ const InvitationCard = () => {
           transition={{ duration: 0.5 }}
           className=" sm:w-[80%] w-[90%] sm:h-[65vh] h-[60%] flex flex-col items-center sm:justify-start justify-between sm:py-0 py-4 "
         >
-          <h1 className="sm:text-5xl text-2xl font-bold relative sm:mb-20 mb-8 text-[#1C110D]">
+          <h1 className="sm:text-3xl text-2xl font-bold relative sm:mb-10 mb-8 text-[#1C110D]">
             सादर आमंत्रण{" "}
             <span className="inline-block w-full bg-[#1C110D] h-[0.1em] absolute bottom-[-15%] left-0"></span>
           </h1>
           <p className="text-center sm:text-4xl text-lg leading-6 sm:leading-[2.8rem] sm:mb-12 mb-4 text-[#563F2F] sm:font-normal font-bold ">
-            सम्माननीय पार्टनर्स, {(name)?`${name},`:""} <br />
-            समस्त प्रथम वर्ष के आर्टिकल्स की ओर से आयोजित उक्त स्नेह मिलन <br />
+            सम्माननीय <span className="font-bold">{(name)?`${name} जी ,`:""}</span> <br />
+            समस्त प्रथम वर्ष के आर्टिकल्स की ओर से <br /> आयोजित उक्त स्नेह मिलन <br />
             कार्यक्रम में आप सपरिवार सादर आमंत्रित हैं। <br />
             कृपया, पधारकर हमें अनुगृहीत करें।
           </p>
-          <div className="w-[20vmin] h-[0.01em] bg-[#563F2F] mb-4"></div>
+          <div className="w-[20vmin] h-[0.01em] bg-[#563F2F] mb-2"></div>
           <h5 className="text-center sm:text-2xl text-sm sm:mb-8 mb-4 text-[#513A2E] font-bold">
             शनिवार, 10 फरवरी, 2024 <br />
             प्रात: 10.00 से सायं 8.00 बजे तक <br />
-            बंसारी ग्रीन्स रिज़ॉर्ट, अलुवा, अहमदाबाद
+            बंसरी ग्रीन्स रिज़ॉर्ट, अलुवा, <br />
+             अहमदाबाद
           </h5>
-          <div className="w-[20vmin] h-[0.01em] bg-[#563F2F] mb-4"></div>
+          <div className="w-[20vmin] h-[0.01em] bg-[#563F2F] mb-2"></div>
 
-          <h6 className="text-center sm:text-2xl text-sm text-[#563F2F] sm:font-normal font-bold">
-            विनयावनत <br /> समस्त आर्टिकल्स, प्रथम वर्ष, <br />
-            तलाटी एंड तलाटी
+          <h6 className="text-center sm:text-2xl text-sm text-[#563F2F] font-bold">
+            विनयावनत <br /> आपके प्रिये आर्टिकल्स, प्रथम वर्ष, <br />
+            तलाटी एंड तलाटी एलएलपी
           </h6>
         </motion.div>
       </div>
